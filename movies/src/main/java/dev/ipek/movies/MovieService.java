3cf427671b020a0d310850be1d16041ee6345f59
ipekozbay <20060401@stu.omu.edu.tr>
2023-05-30 22:47:07 +0300

    New GET Http request written

--- movies/src/main/java/dev/ipek/movies/MovieService.java
+++ movies/src/main/java/dev/ipek/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.ipek.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,5 +15,9 @@ public class MovieService {
     public List<Movie> allMovies() {
         System.out.println(movieRepository.findAll().toString());
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id){
+        return  movieRepository.findById(id);
     }
 }
