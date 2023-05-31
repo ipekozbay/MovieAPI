8f9d9fe7301d30d92c7ecc53f5f7dc5fb728e5df
ipekozbay <20060401@stu.omu.edu.tr>
2023-05-31 23:28:06 +0300

    31.05.2023

--- movies/src/main/java/dev/ipek/movies/MovieRepository.java
+++ movies/src/main/java/dev/ipek/movies/MovieRepository.java
@@ -3,5 +3,9 @@ package dev.ipek.movies;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+
 }
