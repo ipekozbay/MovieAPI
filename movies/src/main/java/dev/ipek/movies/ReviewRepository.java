602fdbe8278eeffd2579a4b4af41942b5e7fc5a3
ipekozbay <20060401@stu.omu.edu.tr>
2023-06-02 23:15:10 +0300

    02.05.2023

---
+++ movies/src/main/java/dev/ipek/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.ipek.movies;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ movies/src/main/java/dev/ipek/movies/ReviewService.java
@@ -0,0 +1,22 @@
+package dev.ipek.movies;
+
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    private ReviewRepository reviewRepository ;
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId){
+        Review review = new Review(reviewBody);
+        reviewRepository.insert(review);
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review));
+        
+    }
+}
